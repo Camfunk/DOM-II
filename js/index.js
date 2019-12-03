d5a9cb8f4a255de4dc63565001f434d090d6fc37
camfunk <cameronfunk92@gmail.com>
2019-12-03 17:57:01 -0500

    added click events and stop props

--- js/index.js
+++ js/index.js
@@ -2,16 +2,47 @@
 
 //mouseEnter
 const imgUp = document.querySelectorAll(".img").forEach(element => {
-  element.addEventListener("mouseenter", function() {
-    element.style.transform = "scale(1.2)";
+  element.addEventListener("mouseenter", () => {
+    element.style.transform = "scale(2.5)";
     element.style.transition = "transform 0.3s";
   });
 });
 
 //mouseLeave
 const imgDown = document.querySelectorAll(".img").forEach(element => {
-  element.addEventListener("mouseleave", function() {
+  element.addEventListener("mouseleave", () => {
     element.style.transform = "scale(1.0)";
     element.style.transition = "transform 0.3s";
   });
 });
+
+//on click
+const navColor = document.querySelector(".main-navigation");
+navColor.addEventListener("click", e => {
+  navColor.style.backgroundColor = "seagreen";
+  navColor.style.transition = "1.7s";
+  e.stopPropagation();
+});
+
+const bodyColor = document.querySelector("body");
+bodyColor.addEventListener("click", e => {
+  bodyColor.style.backgroundColor = "teal";
+  bodyColor.style.transition = "1.7s";
+  e.stopPropagation();
+});
+
+// double click
+const navColorReturn = document.querySelector(".main-navigation");
+navColorReturn.addEventListener("dblclick", e => {
+  navColorReturn.style.backgroundColor = "white";
+  e.stopPropagation();
+});
+
+const bodyColorReturn = document.querySelector("body");
+bodyColorReturn.addEventListener("dblclick", () => {
+  bodyColorReturn.style.backgroundColor = "white";
+});
+
+// mouseWheel
+
+//const wheel = document.querySelector("img");
