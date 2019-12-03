947767343f016695fac7b0388204571fbf6f05ef
camfunk <cameronfunk92@gmail.com>
2019-12-03 17:09:11 -0500

    added mouseEnter and mouseLeave to all images with loop

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+
+//mouseEnter
+const imgUp = document.querySelectorAll(".img").forEach(element => {
+  element.addEventListener("mouseenter", function() {
+    element.style.transform = "scale(1.2)";
+    element.style.transition = "transform 0.3s";
+  });
+});
+
+//mouseLeave
+const imgDown = document.querySelectorAll(".img").forEach(element => {
+  element.addEventListener("mouseleave", function() {
+    element.style.transform = "scale(1.0)";
+    element.style.transition = "transform 0.3s";
+  });
+});
